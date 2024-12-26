d7641108d7574f4af5ada4e8a5471fd6e5c6f138
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-12-26 17:58:36 +0700

    Add files via upload

--- sesion22_bt1.c
+++ sesion22_bt1.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+
 struct SinhVien {
-    int id;
-    int age;
+    char id[20];
     char name[50];
+    int age;
 };
-void printfSV(struct SinhVien *sv, int size);
-void addSV(struct SinhVien *sv, int *size);
-void editSV(struct SinhVien *sv, int size);
-void deleteSV(struct SinhVien *sv, int *size);
-void writeToFile(struct SinhVien *sv, int size);
-void sortSV(struct SinhVien *sv, int size);
-void searchSV(struct SinhVien *sv, int size);
+
+void printfSV();
+void loadFileSv();
+void addSV();
+void saveFile();
+void editSV(struct SinhVien *sv1, int numberStudent);
+void deleteSV();
+void seachSV(struct SinhVien *sv1, int numberStudent);
+void sortSV(struct SinhVien *sv, int numberStudent);
+
+struct SinhVien sv[100];
+int numberStudent = 0;
+
 int main() {
-    struct SinhVien sv[100];
-    int size = 0;
     int choice;
-    FILE *file;
+    loadFileSv();
     do {
-        printf("**********MENU***********\n");
+        printf("\n**********MENU***********\n");
         printf("1. In danh sach sinh vien \n");
         printf("2. Them sinh vien\n");
         printf("3. Sua thong tin sinh vien\n");
@@ -32,26 +36,25 @@ int main() {
         getchar(); 
         switch(choice) {
             case 1:
-                printfSV(sv, size);
+                printfSV();
                 break;
             case 2:
-                addSV(sv, &size);
-                writeToFile(sv, size); 
+                addSV();
+                saveFile();
                 break;
             case 3:
-                editSV(sv, size);
-                writeToFile(sv, size); 
+                editSV(sv, numberStudent);
+                saveFile();
                 break;
             case 4:
-                deleteSV(sv, &size);
-                writeToFile(sv, size); 
+                deleteSV();
+                saveFile();
                 break;
             case 5:
-                searchSV(sv, size);
+                seachSV(sv, numberStudent);
                 break;
             case 6:
-                sortSV(sv, size);
-                writeToFile(sv, size);  
+                sortSV(sv, numberStudent);
                 break;
             case 7:
                 printf("Da thoat chuong trinh\n");
@@ -65,86 +68,123 @@ int main() {
     return 0;
 }
 
-void printfSV(struct SinhVien *sv, int size) {
-    if (size == 0) {
+void printfSV() {
+    if (numberStudent == 0) {
         printf("Danh sach sinh vien rong!\n");
         return;
     }
-    printf("Danh sach sinh vien:\n");
-    printf("ID\tName\tAge\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d\t%s\t%d\n", sv[i].id, sv[i].name, sv[i].age);
+    for(int i = 0; i < numberStudent; i++) {
+        printf("Danh sach sinh vien:\n");
+        printf("%s\t", sv[i].name);
+        printf("%s\t", sv[i].id);
+        printf("%d\t", sv[i].age);
+        printf("\n");
     }
 }
-void addSV(struct SinhVien *sv, int *size) {
-    if (*size >= 100) {
+
+void loadFileSv(){
+    FILE *file = fopen("student.bin", "rb");
+    if (file == NULL) {
+        printf("Loi khong the mo file\n");
+        return;
+    }
+    numberStudent = fread(&sv, sizeof(struct SinhVien), 100, file);
+    fclose(file);
+}
+
+void addSV() {
+    if (numberStudent >= 100) {
         printf("Danh sach sinh vien da day, khong the them moi!\n");
         return;
     }
-    struct SinhVien newSV;
+    struct SinhVien sv1;
     printf("Nhap ID: ");
-    scanf("%d", &newSV.id);
-    getchar(); 
+    fgets(sv1.id, sizeof(sv1.id), stdin);
+    sv1.id[strcspn(sv1.id, "\n")] = 0; 
     printf("Nhap ten: ");
-    fgets(newSV.name, sizeof(newSV.name), stdin);
-    newSV.name[strcspn(newSV.name, "\n")] = 0;  
+    fgets(sv1.name, sizeof(sv1.name), stdin);
+    sv1.name[strcspn(sv1.name, "\n")] = 0; 
     printf("Nhap tuoi: ");
-    scanf("%d", &newSV.age);
-    sv[*size] = newSV;
-    (*size)++;
+    scanf("%d", &sv1.age);
+    getchar(); 
+    sv[numberStudent] = sv1;
+    numberStudent++;
     printf("Sinh vien da duoc them.\n");
 }
-void editSV(struct SinhVien *sv, int size) {
-    int id;
+
+void saveFile() {
+    FILE *file;
+    file = fopen("student.bin", "wb");
+    if (file == NULL) {
+        printf("Loi khi luu file\n");
+        return;
+    }
+    fwrite(sv, sizeof(struct SinhVien), numberStudent, file);  
+    fclose(file);
+}
+
+void editSV(struct SinhVien *sv1, int numberStudent) {
+    char id[20];
     printf("Nhap ID sinh vien can sua: ");
-    scanf("%d", &id);
-    for (int i = 0; i < size; i++) {
-        if (sv[i].id == id) {
-            printf("Sinh vien tim thay: %d, %s, %d\n", sv[i].id, sv[i].name, sv[i].age);
+    fgets(id, sizeof(id), stdin);
+    id[strcspn(id, "\n")] = 0; 
+    for (int i = 0; i < numberStudent; i++) {
+        if (strcmp(sv[i].id, id) == 0) { 
+            printf("Sinh vien tim thay: %s, %s, %d\n", sv[i].id, sv[i].name, sv[i].age);
             printf("Cap nhat ten moi: ");
-            getchar();  
             fgets(sv[i].name, sizeof(sv[i].name), stdin);
             sv[i].name[strcspn(sv[i].name, "\n")] = 0; 
             printf("Cap nhat tuoi moi: ");
             scanf("%d", &sv[i].age);
+            getchar(); 
             printf("Thong tin sinh vien da duoc cap nhat.\n");
             return;
         }
     }
-    printf("Sinh vien voi ID %d khong ton tai.\n", id);
+    printf("Sinh vien voi ID %s khong ton tai.\n", id);
 }
-void deleteSV(struct SinhVien *sv, int *size) {
-    int id;
+
+void deleteSV() {
+    char id[20];
     printf("Nhap ID sinh vien can xoa: ");
-    scanf("%d", &id);
-    for (int i = 0; i < *size; i++) {
-        if (sv[i].id == id) {
-            for (int j = i; j < *size - 1; j++) {
-                sv[j] = sv[j + 1];
+    fgets(id, sizeof(id), stdin);
+    id[strcspn(id, "\n")] = 0;
+    for (int i = 0; i < numberStudent; i++) {
+        if (strcmp(sv[i].id, id) == 0) {
+            for (int j = i; j < numberStudent - 1; j++) {
+                sv[j] = sv[j + 1]; 
             }
-            (*size)--;
+            numberStudent--;  
             printf("Sinh vien da duoc xoa.\n");
             return;
         }
     }
-    printf("Sinh vien %d khong ton tai.\n", id);
+    printf("Sinh vien voi ID %s khong ton tai.\n", id);
 }
-void searchSV(struct SinhVien *sv, int size) {
-    int id;
-    printf("Nhap ID sinh vien can tim: ");
-    scanf("%d", &id);
-    for (int i = 0; i < size; i++) {
-        if (sv[i].id == id) {
-            printf("Sinh vien tim thay: %d, %s, %d\n", sv[i].id, sv[i].name, sv[i].age);
-            return;
+
+void seachSV(struct SinhVien *sv1, int numberStudent) {
+    char id[20];
+    int found = 0;
+    printf("Moi ban nhap Ma sinh vien de tien hanh tim kiem: ");
+    fgets(id, sizeof(id), stdin);
+    id[strcspn(id, "\n")] = 0;
+    for (int i = 0; i < numberStudent; i++) {
+        if (strcmp(sv[i].id, id) == 0) {
+            printf("Sinh vien tim thay la: \n");
+            printf("ID: %s\tTen: %s\tTuoi: %d\n", sv[i].id, sv[i].name, sv[i].age);
+            found = 1;
+            break;
         }
     }
-    printf("Sinh vien ID %d khong ton tai.\n", id);
+    if (!found) {
+        printf("Khong tim thay thong tin sinh vien %s\n", id);
+    }
 }
-void sortSV(struct SinhVien *sv, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (sv[i].id > sv[j].id) {
+
+void sortSV(struct SinhVien *sv, int numberStudent) {
+    for (int i = 0; i < numberStudent - 1; i++) {
+        for (int j = i + 1; j < numberStudent; j++) {
+            if (strcmp(sv[i].id, sv[j].id) > 0) {
                 struct SinhVien temp = sv[i];
                 sv[i] = sv[j];
                 sv[j] = temp;
@@ -153,13 +193,4 @@ void sortSV(struct SinhVien *sv, int size) {
     }
     printf("Danh sach sinh vien da duoc sap xep theo ID.\n");
 }
-void writeToFile(struct SinhVien *sv, int size) {
-    FILE *file = fopen("students.bin", "wb");
-    if (file == NULL) {
-        printf("Loi khi mo file!\n");
-        return;
-    }
-    fwrite(&size, sizeof(int), 1, file);  
-    fwrite(sv, sizeof(struct SinhVien), size, file);  
-    fclose(file);
-}
+
